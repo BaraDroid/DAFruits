f782ebbdef463414dda9fe2fdfde273db0f507bc
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-13 14:41:45 +0100

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', component: FruitlistComponent },   //falls path leer, ist es Anfang
+];      //alles was hier eingegeben ist, kommt nicht in app.component.html
